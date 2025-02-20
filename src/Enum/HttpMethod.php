90b49c846b853113b4bd2820139635f052b69b97
Vladislav <fcritic@users.noreply.github.com>
2025-02-20 20:19:22 +0300

    implementation of http methods

---
+++ src/Enum/HttpMethod.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+namespace AmoJo\Enum;
+
+class HttpMethod
+{
+    /** @var string */
+    public const GET_REQUEST = 'GET';
+
+    /** @var string */
+    public const POST_REQUEST = 'POST';
+
+    /** @var string */
+    public const DELETE_REQUEST = 'DELETE';
+}
